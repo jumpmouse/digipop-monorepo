340ecaec8c001d2dd6930bea5c310967dc4a32a3
Milos <milosbr@gmail.com>
2020-03-26 23:33:36 +0100

    Setup format and lint of projects (#2)
    * prettier config
    
    * Add new scripts and packages needed by the apps
    
    * Load prettier tslint config
    
    * disable eslint rules in server.ts
    
    * update proxy config for admin project

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,10 +2,10 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
+/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-explicit-any */
 const express = require('express');
 const bodyParser = require('body-parser');
-
+/* eslint-enable @typescript-eslint/no-var-requires */
 const app = express();
 const PORT = process.env.PORT || process.env.APP_PORT || 80;
 
@@ -19,15 +19,24 @@ app.get(
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/api/*', (req: any, res: any) => {
-  res.send({ data: `data recieved: api get` });
-});
+app.get(
+  '/api/*',
+  (req: any, res: { send: (arg0: { data: string }) => void }) => {
+    res.send({ data: `data recieved: api get` });
+  }
+);
 
-app.post('/api/*', (req: any, res: any) => {
-  res.send({ data: `data recieved: ${req.body.param}` });
-});
+app.post(
+  '/api/*',
+  (
+    req: { body: { param: any } },
+    res: { send: (arg0: { data: string }) => void }
+  ) => {
+    res.send({ data: `data recieved: ${req.body.param}` });
+  }
+);
 
-app.route('*').get((req: any, res: any) => {
+app.route('*').get((req: any, res: { sendFile: (arg0: string) => void }) => {
   res.sendFile(__dirname + '/index.html');
 });
 
