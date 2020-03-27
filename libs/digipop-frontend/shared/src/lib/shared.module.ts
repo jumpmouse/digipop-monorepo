10b215c7415dcea198320a3ef7b36902a05a5ea5
Milos <milosbr@gmail.com>
2020-03-27 18:48:11 +0100

    Add files to shared lib

--- libs/digipop-frontend/shared/src/lib/shared.module.ts
+++ libs/digipop-frontend/shared/src/lib/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { CardsModule } from 'angular-bootstrap-md';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LoaderComponent } from './components/loader/loader.component';
 import { ProjectsListComponent } from './components/projects-list/projects-list.component';
+import { RouterModule } from '@angular/router';
 import { EditableProjectComponent } from './components/editable-project/editable-project.component';
 
 @NgModule({
