3cd00bb9f74eb737db530bd7df1be130282563cc
Milos <milosbr@gmail.com>
2020-03-27 18:51:16 +0100

    update prefix in shared lib

--- libs/digipop-frontend/shared/src/lib/components/loader/loader.component.ts
+++ libs/digipop-frontend/shared/src/lib/components/loader/loader.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'digipop-admin-loader',
+  selector: 'digipop-shared-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss']
 })
