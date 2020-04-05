f72dbbecf74e5f0444d42defb441cc87f0c992c6
Milos <milosbr@gmail.com>
2020-04-05 23:15:23 +0200

    Use resolver on home route

--- apps/frontend/src/app/home/home-routing.module.ts
+++ apps/frontend/src/app/home/home-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { extract } from '@digipop/core';
+import { extract, ResolverGuard } from '@digipop/core';
 import { ContentManagementComponent } from './home.component';
 import { Shell } from '@app/shell/shell.service';
 
@@ -11,7 +11,10 @@ const routes: Routes = [
       path: '',
       component: ContentManagementComponent,
       data: { title: extract('Home') },
-      pathMatch: 'full'
+      pathMatch: 'full',
+      resolve: {
+        script: ResolverGuard
+      }
     }
   ])
 ];
