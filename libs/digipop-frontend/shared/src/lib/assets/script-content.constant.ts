13c4209c570631d91bed6636e484145f128fabca
Milos <milosbr@gmail.com>
2020-04-05 17:57:14 +0200

    Update script-content.constant.ts

--- libs/digipop-frontend/shared/src/lib/assets/script-content.constant.ts
+++ libs/digipop-frontend/shared/src/lib/assets/script-content.constant.ts
@@ -10,94 +10,91 @@ export const SadrzajSkripte: Skripta = {
   opis:
     'Opširan opis skripte. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   predmeti: {
-    I: {
-      id: 'I',
+    pez: {
+      id: 'pez',
       naziv: 'Populaciona ekologija životinja',
       kratki_opis:
         'Kratki opis predmeta. U dva reda, na primer. Mozda moze i u tri reda.',
       opis:
         'Dugacki opis predmeta. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-      link: 'I_populaciona-ekologija-zivotinja',
+      link: 'pez_populaciona-ekologija-zivotinja',
       URL_slike: '',
       oblasti: {
-        'I-1': {
-          id: 'I-1',
+        'pez-I': {
+          id: 'pez-I',
           naziv: 'Demografija',
           opis: 'opis oblasti',
-          link: 'I-1_demografija',
+          link: 'pez-I_demografija',
           URL_slike: '',
           programske_celine: {
-            'I-1-1': {
-              id: 'I-1-1',
+            'pez-I-1': {
+              id: 'pez-I-1',
               naziv: 'Uvod',
-              link: 'I-1-1_uvod',
+              link: 'pez-I-1_uvod',
               URL_slike: '',
               tekst: 'uvodni tekst ovde',
-              podceline: {}
+              redosled: '1'
             },
-            'I-1-2': {
-              id: 'I-1-2',
+            'pez-I-2': {
+              id: 'pez-I-2',
               naziv: 'Demografske tablice',
-              link: 'I-1-2_demografske-tablice',
+              link: 'pez-I-2_demografske-tablice',
               URL_slike: '',
               tekst: 'tekst demografskih tablica',
-              podceline: {}
+              redosled: '2'
             },
-            'I-1-3': {
-              id: 'I-1-3',
+            'pez-I-3': {
+              id: 'pez-I-3',
               naziv: 'Mortalitet',
-              link: 'I-1-3_mortalitet',
+              link: 'pez-I-3_mortalitet',
               URL_slike: '',
               tekst: 'tekst o mortalitetu',
-              podceline: {
-                'I-1-3-1': {
-                  id: 'I-1-3-1',
-                  naziv: 'Tipovi preživljavanja',
-                  link: 'I-1-3-1_tipovi-prezivljavanja',
-                  URL_slike: '',
-                  tekst: 'tekst o tipovima prezivljavanja',
-                  podceline: {}
-                }
-              }
+              redosled: '3'
             },
-            'I-1-4': {
-              id: 'I-1-4',
+            'pez-I-4': {
+              id: 'pez-I-4',
+              naziv: 'Tipovi preživljavanja',
+              link: 'pez-I-4_tipovi-prezivljavanja',
+              URL_slike: '',
+              tekst: 'tekst o tipovima prezivljavanja',
+              redosled: '3.1'
+            },
+            'pez-I-5': {
+              id: 'pez-I-5',
               naziv: 'Natalitet',
-              link: 'I-1-4_natalitet',
+              link: 'pez-I-5_natalitet',
               URL_slike: '',
               tekst: 'natalitet tekst',
-              podceline: {}
+              redosled: '4'
             },
-            'I-1-5': {
-              id: 'I-1-5',
+            'pez-I-6': {
+              id: 'pez-I-6',
               naziv: 'Uzrasna struktura',
-              link: 'I-1-5_uzrasna-struktura',
+              link: 'pez-I-6_uzrasna-struktura',
               URL_slike: '',
               tekst: 'tekst o uzrasnoj strukturi',
-              podceline: {
-                'I-1-5-1': {
-                  id: 'I-1-5-1',
-                  naziv: 'Dostizanje stabilne uzrasne strukture',
-                  link: 'I-1-5-1_dostizanje-stabilne-uzrasne-strukture',
-                  URL_slike: '',
-                  tekst:
-                    'tekst o tome kako se dostize stabilna uzrasna struktura',
-                  podceline: {}
-                }
-              }
+              redosled: '5'
+            },
+            'pez-I-7': {
+              id: 'pez-I-7',
+              naziv: 'Dostizanje stabilne uzrasne strukture',
+              link: 'pez-I-7_dostizanje-stabilne-uzrasne-strukture',
+              URL_slike: '',
+              tekst: 'tekst o tome kako se dostize stabilna uzrasna struktura',
+              redosled: '5.1'
             }
           }
         }
       }
     },
-    II: {
-      id: 'II',
+    oez: {
+      id: 'oez',
       naziv: 'Opšta ekologija životinja',
       kratki_opis:
         'Kratki opis predmeta. U dva reda, na primer. Mozda moze i u tri reda.',
       opis:
         'Dugacki opis predmeta. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-      link: 'II_opsta-ekologija-zivotinja',
+      link: 'oez_opsta-ekologija-zivotinja',
       URL_slike: '',
       oblasti: {}
     }
