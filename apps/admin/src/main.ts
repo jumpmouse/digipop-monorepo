ba950af75d0c1d38fdb5cbffc310b321c1f6ea04
Milos <milosbr@gmail.com>
2020-04-12 00:05:04 +0200

    Fix hmr (#16)
    * use hmr module instead of function in frontend app
    
    * use hmr module instead of function in admin app
    
    * remove unused files

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -9,7 +9,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from '@app/app.module';
 import { environment } from '@env/environment';
-import { hmrBootstrap } from './hmr';
+import { hmrModule } from '@angularclass/hmr';
+
 
 if (environment.production) {
   enableProdMode();
@@ -18,7 +19,14 @@ if (environment.production) {
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
 if (environment.hmr) {
-  hmrBootstrap(module, bootstrap);
+  if (module[ 'hot' ]) {
+    bootstrap().then((ngModuleRef) => {
+      return hmrModule(ngModuleRef, module);
+    }).catch(err => console.log(err));
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
 } else {
   bootstrap().catch(err => console.error(err));
 }
